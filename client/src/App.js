e52d98d6e2602a52b2ac6312a7687b4efe4dcd6d
KurnosovaElena <138401728+KurnosovaElena@users.noreply.github.com>
2024-02-21 22:47:21 +0300

    feat: add pages and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.scss';
+import { Main } from './pages/Main/Main';
+import { Specialists } from './pages/Specialists/Specialists';
+import { Contacts } from './pages/Contacts/Contacts';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/main" element={<Main />} />
+          <Route path="/specialists" element={<Specialists />} />
+          <Route path="/contacts" element={<Contacts />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
